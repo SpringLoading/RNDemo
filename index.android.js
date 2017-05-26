cdfd43d372737433aa85572bf5ceb5298bc34600
qianyipeng <loveqianwithyang@gmail.com>
2017-05-26 17:43:14 +0800

    页面跳转

--- index.android.js
+++ index.android.js
@@ -11,139 +11,54 @@ import {
   Text,
   View,
   Image,
-    TextInput,
-ListView
+  TextInput,
+    ListView,
+    ScrollView,
+    Animated,
+    Button,
+
 } from 'react-native';
-class Blink extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { showText: true };
 
-        // 每1000毫秒对showText状态做一次取反操作
-        setInterval(() => {
-            this.setState({ showText: !this.state.showText });
-        }, 1000);
-    }
-
-
+import { StackNavigator } from 'react-navigation';
+class MainScreen extends React.Component {
+    static navigationOptions = {
+        title: '第一个页面的标题',
+    };
     render() {
-        // 根据当前showText的值决定是否显示text内容
-        let display = this.state.showText ? this.props.text : ' ';
+        const { navigate } = this.props.navigation;
         return (
-            <Text>{display}</Text>
+            <Button
+                title="点击跳转到第二个页面"
+                onPress={() =>
+                    navigate('Profile', { name: 'Jane' })
+                }
+            />
         );
     }
 }
 
 
-class FlexDirectionBasics extends Component {
+class ProfileScreen extends React.Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: `Chat with ${navigation.state.params.name}`,
+    });
     render() {
+        const { params } = this.props.navigation.state;
+        const { navigate } = this.props.navigation;
         return (
-            <View style={{
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'flex-start',
-            }}>
-                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-            </View>
-        );
-    }
-};
+                <Text>Chat with {params.name}</Text>,
+                <Button onPress={()=>navigate('Main')}
+                        title= {params.name}/>
 
-class PizzaTranslator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {text: ''};
-    }
-
-    render() {
-        return (
-            <View style={{padding: 10}}>
-                <TextInput
-                    style={{height: 40, width:100}}
-                    placeholder="Type here to translate!"
-                    onChangeText={(text) => this.setState({text})}
-                />
-                <Text style={{padding: 10, fontSize: 42}}>
-                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-                </Text>
-            </View>
         );
     }
 }
-
-class ListViewBasics extends Component {
-    // 初始化模拟数据
-    constructor(props) {
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-            dataSource: ds.cloneWithRows([
-                'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
-            ])
-        };
-    }
-    render() {
-        return (
-            <View style={{flex: 1, paddingTop: 22}}>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Text>{rowData}</Text>}
-                />
-            </View>
-        );
-    }
-}
-
-export default class RNDemo extends Component {
-  render() {
-      let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-      };
-
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          我的第一个RN程序
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Image source={pic} style={{width:100,height:100}}></Image>
-        <Image source={require('./images/icon-logo.png')} style={{width:100,height:100}}></Image>
-
-          <Blink text="显示和隐藏"/>
-        <FlexDirectionBasics ></FlexDirectionBasics>
-        <PizzaTranslator/>
-          <ListViewBasics/>
-      </View>
-
-
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const RNDemo = StackNavigator({
+    Main: {screen: MainScreen},
+    Profile: {screen: ProfileScreen},
 });
+
+
+
 
 AppRegistry.registerComponent('RNDemo', () => RNDemo);
